a42adffb9957cd8b53e820c114b34e64ad37cf62
Kukzee <marlon.rcruz@gmail.com>
2014-07-05 00:39:29 +0800

    T

---
+++ HelloWorld/src/helloworld/kukzee/com/HelloWorld.java
@@ -0,0 +1,10 @@
+package helloworld.kukzee.com;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
