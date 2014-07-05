0af1bb3788ae07c0611a60c7a7adf3a4474f1073
Kukzee <marlon.rcruz@gmail.com>
2014-07-05 23:09:15 +0800

    sandbox

---
+++ HelloWorld/src/helloworld/kukzee/com/StringOperation.java
@@ -0,0 +1,13 @@
+package helloworld.kukzee.com;
+
+public class StringOperation {
+	public static void main(String[] args) {
+		int years = 3, principal=5;
+		System.out.print("After ");
+		System.out.print(years);
+		System.out.print(" years, the value is ");
+		System.out.print(principal);
+		System.out.print("\nAfter " + years + 
+				" years, the value is " + 'p');
+	}
+}
